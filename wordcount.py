7f2237200b75abde865fafa6bf35ad22dc7d93ca
Amber Orrange <orrangeamber@gmail.com>
2021-10-27 18:43:33 -0500

    finished word count

--- wordcount.py
+++ wordcount.py
@@ -2,3 +2,20 @@
 
 
 # put your code here.
+
+word_dict = {}
+def count_words(filename):
+    data = open(filename)
+
+    for line in data:
+        line = line.rstrip().split(" ")
+
+        for word in line:
+            word_dict[word] = word_dict.get(word, 0) + 1
+    
+    for item, count in word_dict.items():
+        print(f"{item} {count}")
+
+
+
+count_words("test.txt")
